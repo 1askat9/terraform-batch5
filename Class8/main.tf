58ec602bc16b62222ddfc2014934ee84b43ed25e
1askat9 <esenkanovaskatwork@gmail.com>
2024-04-20 18:30:10 +0000

    added changes

--- Class8/main.tf
+++ Class8/main.tf
@@ -2,9 +2,13 @@ provider aws {
     region = "us-east-2"
 }
 
+provider aws {
+    region = "us-east-2"
+}
+
 module "vpc" {
-    source = "1askat9/vpcbatch5/aws"
-    version = "0.0.3"
+    source = "kaizenacademy/vpcbatch5/aws"
+    version = "0.0.4"
     region = "us-east-2"
     vpc_cidr = "10.0.0.0/16"
     subnet1_cidr = "10.0.1.0/24"
